558d431653d51fb0e6050e3dd58ce315815d608a
AngelGUst <20233tn103@utez.edu.mx>
2025-11-18 13:16:42 -0600

    Add excursion creation modal and dialog UI
    Introduces a reusable dialog component using Radix UI and a modal for creating excursions. Moves Excursion type to its own file for better organization and updates Excursionpage to include the new modal. Also adds @radix-ui/react-dialog dependency.

--- src/app/Views/Excursiones/Excursionpage.tsx
+++ src/app/Views/Excursiones/Excursionpage.tsx
@@ -1,10 +1,10 @@
-import { Car } from 'lucide-react'
 import React from 'react'
 import CardExcursions from '@/modules/Excursions/cardExcursions'
-
+import ModalExcursionCreate from '@/modules/Excursions/modalExcursionCreate'
 export default function page() {
   return (
     <div>
+      <ModalExcursionCreate></ModalExcursionCreate>
         <CardExcursions />
     </div>
   )
--- src/modules/Excursions/service.ts
+++ src/modules/Excursions/service.ts
@@ -1,23 +1,10 @@
 import { createClient } from "@/lib/Supabase/server";
 
 export async function createExcurtions() {
+    const supabase = await createClient();
     
 }
 
-export type Excursion={
-    id: number;
-    title: string;
-    description?: string;
-    price: string|number;
-    min_deposit?: string|number|null;
-    duration_days?: number|null|string;
-    start_dates?: string|null|number;
-    end_dates?: string|null|number;
-    available_seats?: number|null|string;
-    status?: string|null;
-    created_at?: string|null;
-    updated_at?: string|null;
-}
 export async function getExcursions() {
     const supabase = await createClient();
     const { data, error } = await supabase.from('excursions').select('*');
